0089dc9b2a15d381f08048512ee35f43af843a3c
osemenjoy <136025110+osemenjoy@users.noreply.github.com>
2023-12-11 15:56:18 +0000

    My first commit

---
+++ calculator.py
@@ -0,0 +1,3 @@
+print("Enter '+' for addition.\n Enter '-' for subtraction.\n Enter '/' for division." )
+print("Enter '*' for multiplication.")
+operator = input("Enter the operator (+, -, /, *): ")
