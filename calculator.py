1fa99611859229b16087f37160fe0e7d80b8063e
osemenjoy <136025110+osemenjoy@users.noreply.github.com>
2023-12-12 11:27:25 +0000

    update 1

--- calculator.py
+++ calculator.py
@@ -1,3 +1,14 @@
 print("Enter '+' for addition.\n Enter '-' for subtraction.\n Enter '/' for division." )
 print("Enter '*' for multiplication.")
 operator = input("Enter the operator (+, -, /, *): ")
+num1 = float(input("Enter First number: "))
+num2 = float(input("Enter Second number: "))
+if operator == '+':
+    sum = num1 + num2
+    print(sum)
+elif operator == '-':
+    subtraction = num1 - num2
+elif operator == '/':
+    division = num1/num2
+elif operator == '*':
+    multiplication = num1 * num2
